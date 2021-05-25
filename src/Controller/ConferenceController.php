6f48d9d4c35244008ce9134adbcc6cce9ef7dcb5
rekno9 <arturas.spn@gmail.com>
2021-05-25 18:15:09 +0300

    'last'

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(Request $request): Response
+    {
+        return new Response(
+            <<<EOF
+            <html>
+                <body>
+                    <img src="/images/under-maint.jpg" />
+                </body>
+            </html>
+            EOF
+        );
+    }
+}
