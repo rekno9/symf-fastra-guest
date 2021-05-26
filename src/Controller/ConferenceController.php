5ca195215043b8037a32d164ceff8a17875b49eb
rekno9 <arturas.spn@gmail.com>
2021-05-26 12:46:46 +0300

    Added twig dependacy in addition to basic pages(index,show)

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,26 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(Request $request): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(
-            <<<EOF
-            <html>
-                <body>
-                    <img src="/images/under-maint.jpg" />
-                </body>
-            </html>
-            EOF
-        );
+        return new Response($twig->render('conference/index.html.twig',
+            ['conferences' => $conferenceRepository->findAll(),]
+        ));
+    }
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository){
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+        ]));
     }
 }
