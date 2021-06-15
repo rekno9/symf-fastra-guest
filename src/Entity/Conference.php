42fbab4ba3c9b3166e8301b104336df642fb368a
rekno9 <arturas.spn@gmail.com>
2021-06-15 16:23:06 +0300

    Adding conference entity listener to work with automatc slug computing

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -7,6 +7,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
@@ -59,6 +61,12 @@ class Conference
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function computeSlug(SluggerInterface $slugger){
+        if(!$this->slug || '-' === $this->slug){
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
     }
 
     public function getCity(): ?string
