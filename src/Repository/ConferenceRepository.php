33e05f0f08258e25ff775b5adbc3b07316fe7fc5
rekno9 <arturas.spn@gmail.com>
2021-06-15 16:31:52 +0300

    silly typo fix

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -20,7 +20,7 @@ class ConferenceRepository extends ServiceEntityRepository
     }
 
     public function findAll(){
-        return $this->findAll([], ['year' => 'ASC', 'city' => 'ASC']);
+        return $this->findBy([], ['year' => 'ASC', 'city' => 'ASC']);
     }
 
     // /**
